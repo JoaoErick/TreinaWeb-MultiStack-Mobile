6b6930239b5f17755e568b36aae5000f16f0c9e0
JoaoErick <jerick1415@outlook.com>
2021-06-17 11:25:14 -0300

    Fifth day.

--- src/pages/encontrar-diarista.tsx
+++ src/pages/encontrar-diarista.tsx
@@ -1,23 +1,47 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, ScrollView } from "react-native";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import TextInput from "ui/components/inputs/TextInput/TextInput";
 import { TextInputMask } from "react-native-masked-text";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useTheme } from "@emotion/react";
 import Button from "ui/components/inputs/Button/Button";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 import {
-    FormContainer,
-    TextContainer,
-    ErrorText,
-    ResponseContainer,
-} from '@styles/pages/encontrar-diarista.styled';
+  FormContainer,
+  TextContainer,
+  ErrorText,
+  ResponseContainer,
+} from "@styles/pages/encontrar-diarista.styled";
+import useIndex from "data/hooks/pages/useIndex.page";
+import useEncontrarDiarista from "data/hooks/pages/useEncontrarDiarista.page.mobile";
+
 
 const EncontrarDiaristas: React.FC = () => {
-  const [cep, setCep] = useState("");
+  const { colors } = useTheme();
+  const {
+      cep,
+      setCep,
+      cepValido,
+      buscarProfissionais,
+      erro,
+      buscaFeita,
+      carregando,
+      diaristasRestantes,
+      diaristas,
+    } = useIndex(),
+    { cepAutomatico } = useEncontrarDiarista();
+
+    useEffect(() => {
+      if (cepAutomatico && !cep) {
+        setCep(cepAutomatico);
+        buscarProfissionais(cepAutomatico);
+      }
+    }, [cepAutomatico]);
+
 
   return (
-    <View>
+    <ScrollView>
       <PageTitle
         title={"Conheça os profissionais"}
         subtitle={
@@ -39,18 +63,54 @@ const EncontrarDiaristas: React.FC = () => {
           }}
         />
 
-        <Button mode={"contained"} style={{ marginTop: 32 }}>
+        {erro ? <ErrorText>{erro}</ErrorText> : null}
+
+        <Button
+          mode={"contained"}
+          style={{ marginTop: 32 }}
+          color={colors.accent}
+          disabled={!cepValido || carregando}
+          onPress={() => buscarProfissionais(cep)}
+          loading={carregando}
+        >
           Buscar
         </Button>
       </FormContainer>
 
-      <UserInformation
-        name={"joão Erick barbosa"}
-        rating={3}
-        picture={"https://github.com/JoaoErick.png"}
-        description={"Bahia"}
-      />
-    </View>
+      {buscaFeita &&
+        (diaristas.length > 0 ? (
+          <ResponseContainer>
+            {diaristas.map((item, index) => (
+              <UserInformation
+                key={index}
+                name={item.nome_completo}
+                rating={item.reputacao || 0}
+                picture={item.foto_usuario || ""}
+                description={item.cidade}
+                darker={index % 2 === 1}
+              />
+            ))}
+
+            {diaristasRestantes > 0 && (
+              <TextContainer>
+                ...e mais {diaristasRestantes}{" "}
+                {diaristasRestantes > 1
+                  ? "profissionais atendem"
+                  : "profissional atende"}{" "}
+                ao seu endereço.
+              </TextContainer>
+            )}
+
+            <Button color={colors.accent} mode={"contained"}>
+              Contratar um profissional
+            </Button>
+          </ResponseContainer>
+        ) : (
+          <TextContainer>
+            Ainda não temos nenhuma diarista disponível em sua região
+          </TextContainer>
+        ))}
+    </ScrollView>
   );
 };
 
